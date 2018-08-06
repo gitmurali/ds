29a6e7cf12d78de5f60a0d250281f88112d478c4
Murali Prasanth N <muraliprasanth.n@sonata-software.com>
2018-08-06 18:54:48 +0530

    queue implementation using circular queue

--- src/com/murali/Main.java
+++ src/com/murali/Main.java
@@ -1,8 +1,14 @@
 package com.murali;
 
+import java.util.ArrayDeque;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayDeque<String> strings = new ArrayDeque<>();
+        strings.add("murali");
+        strings.add("prashanth");
+        strings.add("nutalapati");
+        System.out.println(strings.pop());
     }
 }
