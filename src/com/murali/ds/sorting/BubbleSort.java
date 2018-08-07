5621d85e648b4d3f6302396b31967817b6fdc7bf
Murali Nutalapati <info@muraliprashanth.me>
2018-08-07 16:35:58 +0530

    Bubble sort implementation

---
+++ src/com/murali/ds/sorting/BubbleSort.java
@@ -0,0 +1,43 @@
+package com.murali.ds.sorting;
+
+/**
+ * @author muralinutalapati
+ */
+public class BubbleSort {
+
+  private static int list[] = new int[]{3, 5, 6, 8,43,78,100, 10, 1, 2, 4, 7, 9};
+
+  public static void main(String[] args) {
+    print(list);
+    bubbleSort(list);
+    print(list);
+  }
+
+  public static void print(int[] listToSort) {
+    for (int el : listToSort) {
+      System.out.print(el + ",");
+    }
+    System.out.println();
+  }
+
+  public static void swap(int[] listToSort, int iIndex, int jIndex) {
+    int temp = listToSort[iIndex];
+    listToSort[iIndex] = listToSort[jIndex];
+    listToSort[jIndex] = temp;
+  }
+
+  public static void bubbleSort(int[] listToSort) {
+    for (int i = 0; i < listToSort.length; i++) {
+      boolean swapped = false;
+      for (int j = listToSort.length - 1; j > i; j--) {
+        if (listToSort[j] < listToSort[j - 1]) {
+          swap(listToSort, j, j - 1);
+          swapped = true;
+        }
+      }
+      if (!swapped) {
+        break;
+      }
+    }
+  }
+}
