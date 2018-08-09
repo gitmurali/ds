9de310b5c8f804e91ff0a79b0741ae7e28eb6c88
Murali Nutalapati <info@muraliprashanth.me>
2018-08-09 19:20:15 +0530

    merge sort

---
+++ src/com/murali/ds/sorting/MergeSort.java
@@ -0,0 +1,80 @@
+package com.murali.ds.sorting;
+
+/**
+ * @author muralinutalapati
+ */
+public class MergeSort {
+
+  private static int listToSort[] = new int[]{3, 5, 6, 8, 43, 78, 100, 10, 1, 2, 4, 7, 9};
+
+  public static void main(String[] args) {
+    print(listToSort);
+    mergeSort(listToSort);
+  }
+
+  private static void print(int[] listToSort) {
+    for (int el : listToSort) {
+      System.out.print(el + ",");
+    }
+    System.out.println();
+  }
+
+  private static void mergeSort(int[] listToSort) {
+    if (listToSort.length == 1) {
+      return;
+    }
+
+    int midIndex = listToSort.length / 2 + listToSort.length % 2;
+    int[] listFirstHalf = new int[midIndex];
+    int[] listSecondHalf = new int[listToSort.length - midIndex];
+    split(listToSort, listFirstHalf, listSecondHalf);
+
+    mergeSort(listFirstHalf);
+    mergeSort(listSecondHalf);
+
+    merge(listToSort, listFirstHalf, listSecondHalf);
+    print(listToSort);
+  }
+
+  private static void split(int[] listToSort, int[] listFirstHalf, int[] listSecondHalf) {
+    int index = 0;
+    int secondHalfStartIndex = listFirstHalf.length;
+    for (int elements : listToSort) {
+      if (index < secondHalfStartIndex) {
+        listFirstHalf[index] = listToSort[index];
+      } else {
+        listSecondHalf[index - secondHalfStartIndex] = listToSort[index];
+      }
+      index++;
+    }
+  }
+
+  private static void merge(int[] listToSort, int[] listFirstHalf, int[] listSecondHalf) {
+    int mergeIndex = 0;
+    int firstHalfIndex = 0;
+    int secondHalfIndex = 0;
+
+    while (firstHalfIndex < listFirstHalf.length && secondHalfIndex < listSecondHalf.length) {
+      if (listFirstHalf[firstHalfIndex] < listSecondHalf[secondHalfIndex]) {
+        listToSort[mergeIndex] = listFirstHalf[firstHalfIndex];
+        firstHalfIndex++;
+      } else if (secondHalfIndex < listSecondHalf.length) {
+        listToSort[mergeIndex] = listSecondHalf[secondHalfIndex];
+        secondHalfIndex++;
+      }
+      mergeIndex++;
+    }
+
+    if (firstHalfIndex < listFirstHalf.length) {
+      while (mergeIndex < listToSort.length) {
+        listToSort[mergeIndex++] = listFirstHalf[firstHalfIndex++];
+      }
+    }
+    if (secondHalfIndex < listSecondHalf.length) {
+      while (mergeIndex < listToSort.length) {
+        listToSort[mergeIndex++] = listSecondHalf[secondHalfIndex++];
+      }
+    }
+  }
+
+}
