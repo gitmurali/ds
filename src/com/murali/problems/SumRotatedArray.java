72d825b05199e0b8ea0ffd7eafd968871a205156
Murali <murali.nutalapati@bp.com>
2020-05-26 21:52:16 +0100

    sorted rotated given sum array

--- src/com/murali/problems/SumRotatedArray.java
+++ src/com/murali/problems/SumRotatedArray.java
@@ -1,4 +1,31 @@
 package com.murali.problems;
 
 public class SumRotatedArray {
+  public static void main(String[] args) {
+    int arr[] = {11, 15, 1, 8, 9, 10};
+    int x = 16;
+    System.out.println(pairInSortedRotated(arr, arr.length, x));
+  }
+
+  static boolean pairInSortedRotated(int arr[], int n, int x) {
+    int i;
+    for(i = 0;i<n-1;i++){
+      if(arr[i] > arr[i+1]) break;
+    }
+    int l = (i + 1) % n;
+    int r = i;
+    while (l != r) {
+      if(arr[l] + arr[r] == x) {
+        System.out.println(arr[l]);
+        System.out.println(arr[r]);
+        return true;
+      }
+      if(arr[l] + arr[r] > x) {
+        r = (n + r - 1) % n;
+      } else {
+        l = (l + 1) % n;
+      }
+    }
+    return false;
+  }
 }
