d2686747e84d01d0ecd313c0737746b61cf6e543
Murali <info@muraliprashanth.me>
2020-06-17 22:21:38 +0100

    return indices

--- src/com/murali/problems/arrays/LargestSumContiguousSubArray.java
+++ src/com/murali/problems/arrays/LargestSumContiguousSubArray.java
@@ -11,15 +11,21 @@ public class LargestSumContiguousSubArray {
   private static int maxSubArraySum(int[] a) {
     int max_sum_so_far = 0, max_ending_here = 0;
     int size = a.length;
+    int start = 0, end = 0, s=0;
     for (int i = 0; i < size; i++) {
       max_ending_here = max_ending_here + a[i];
       if (max_sum_so_far < max_ending_here) {
         max_sum_so_far = max_ending_here;
+        start=s;
+        end = i;
       }
-      if (max_ending_here < 0)
+      if (max_ending_here < 0) {
         max_ending_here = 0;
-      System.out.println(max_sum_so_far + ":" + i);
+        s = i + 1;
+      }
     }
+    System.out.println("Starting index " + start);
+    System.out.println("Ending index " + end);
     return max_sum_so_far;
   }
 }
