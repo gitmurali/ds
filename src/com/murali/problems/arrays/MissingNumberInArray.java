eac566bbf36b42c0e1b6c46d31cd8164a3b115ed
Murali <info@muraliprashanth.me>
2020-06-24 21:06:37 +0100

    add another example

--- src/com/murali/problems/arrays/MissingNumberInArray.java
+++ src/com/murali/problems/arrays/MissingNumberInArray.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 public class MissingNumberInArray {
   public static void main(String[] args) {
     int[] arr = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+//    int[] arr = {3, 0, 1};
     int n = arr.length + 1;
     int sum = 0;
     for (int i : arr) {
